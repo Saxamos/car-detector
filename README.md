bfd13c64a1cfa6fc89a3856b1b678262699d5c7d
Samuel <samuel.rochette@yahoo.fr>
2019-04-19 01:08:56 +0200

    add entry point and enhance readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # car-detector
 A car detection app for raspberry
+
+* launch the tests using the runner that you prefer
+* launch the app with the command `detect`
+
+#### TODO
+mettre les articles en lien https://blog.octo.com/ia-embarquee-deployer-du-deep-learning-sur-un-raspberry/
--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup(name='car-detector', long_description=open('README.md').read())
+setup(name='car-detector',
+      long_description=open('README.md').read(),
+      entry_points={
+          'console_scripts': [
+              'detect = app.__main__:main',
+          ],
+      })
